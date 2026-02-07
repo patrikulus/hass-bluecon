3b72659a77e99bd239518ed3030aa96d42bb18d9
Patryk Lotzwi <plotz@equinor.com>
2026-02-07 14:26:12 +0100

    Updated logic

--- custom_components/bluecon/__init__.py
+++ custom_components/bluecon/__init__.py
@@ -1,59 +1,46 @@
-import json
-from .const import CONF_LOCK_STATE_RESET, DOMAIN, SIGNAL_CALL_ENDED, SIGNAL_CALL_STARTED
-from .ConfigFolderOAuthTokenStorage import ConfigFolderOAuthTokenStorage
-from .ConfigFolderNotificationInfoStorage import ConfigFolderNotificationInfoStorage
-from .config_flow import BlueConConfigFlow
-from homeassistant.const import (
-    CONF_API_KEY,
-    CONF_CLIENT_ID,
-    CONF_CLIENT_SECRET,
-    EVENT_HOMEASSISTANT_STOP,
-    Platform
-)
-from homeassistant.helpers.dispatcher import dispatcher_send
-from bluecon import BlueConAPI, INotification, CallNotification, CallEndNotification, IOAuthTokenStorage, INotificationInfoStorage, OAuthToken
-from homeassistant.core import HomeAssistant, callback
+"""The BlueCon integration."""
+import asyncio
+import logging
 from homeassistant.config_entries import ConfigEntry
-from custom_components.bluecon.const import CONF_PACKAGE_NAME, CONF_APP_ID, CONF_PROJECT_ID, CONF_SENDER_ID
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.storage import Store
 
+from .const import DOMAIN
+from .fermax_api import FermaxClient, FermaxAuthError
 
+LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[str] = [Platform.BINARY_SENSOR, Platform.LOCK, Platform.CAMERA, Platform.SENSOR]
+PLATFORMS: list[str] = [Platform.LOCK] # Removed others for now as they might depend on features not in the script
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    def notification_callback(notification: INotification):
-        if type(notification) is CallNotification:
-            dispatcher_send(hass, SIGNAL_CALL_STARTED.format(notification.deviceId, notification.accessDoorKey))
-        elif type(notification) is CallEndNotification:
-            dispatcher_send(hass, SIGNAL_CALL_ENDED.format(notification.deviceId))
+    """Set up BlueCon from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
 
-    hass.data[DOMAIN] = {
-        "bluecon": None
-    }
+    session = async_get_clientsession(hass)
+    store = Store(hass, 1, f"{DOMAIN}.{entry.entry_id}.token")
 
-    bluecon = await BlueConAPI.create_already_authed(
-        entry.data[CONF_CLIENT_ID],
-        entry.data[CONF_CLIENT_SECRET],
-        entry.data.get(CONF_SENDER_ID, None),
-        entry.data.get(CONF_API_KEY, None),
-        entry.data.get(CONF_PROJECT_ID, None),
-        entry.data.get(CONF_APP_ID, None),
-        entry.data.get(CONF_PACKAGE_NAME, None),
-        notification_callback, 
-        ConfigFolderOAuthTokenStorage(hass), 
-        ConfigFolderNotificationInfoStorage(hass)
-    )
+    token_data = await store.async_load()
 
-    if entry.data.get(CONF_SENDER_ID, None) is not None and entry.data.get(CONF_API_KEY, None) is not None and entry.data.get(CONF_PROJECT_ID, None) is not None and entry.data.get(CONF_APP_ID, None) is not None and entry.data.get(CONF_PACKAGE_NAME, None) is not None:
-        bluecon.startNotificationListener()
+    def save_token(token):
+        hass.async_create_task(store.async_save(token))
 
-        @callback
-        async def cleanup(event):
-            await bluecon.stopNotificationListener()
-        
-        hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, cleanup)
+    client = FermaxClient(session, token_data, save_token)
 
-    hass.data[DOMAIN][entry.entry_id] = bluecon
+    try:
+        if not client.token_valid:
+            username = entry.data.get(CONF_USERNAME)
+            password = entry.data.get(CONF_PASSWORD)
+            if username and password:
+                await client.async_login(username, password)
+            else:
+                LOGGER.warning("No credentials found for re-authentication")
+    except FermaxAuthError as err:
+        LOGGER.error("Authentication failed during setup: %s", err)
+        return False
+
+    hass.data[DOMAIN][entry.entry_id] = client
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
@@ -62,55 +49,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 async def update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle options update."""
     await hass.config_entries.async_reload(entry.entry_id)
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
-    
+
     return unload_ok
-
-async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
-    tempNotificationInfoStorage: INotificationInfoStorage = ConfigFolderNotificationInfoStorage(hass)
-    tempOAuthTokenStorage: IOAuthTokenStorage = ConfigFolderOAuthTokenStorage(hass)
-
-    if config_entry.version == 1:
-        try:
-            with open("credentials.json", "r") as f:
-                credentials = json.load(f)
-        except FileNotFoundError:
-            credentials = None
-        
-        try:
-            with open("persistent_ids.txt", "r") as f:
-                persistentIds = [x.strip() for x in f]
-        except FileNotFoundError:
-            persistentIds = None
-        
-        tempOAuthTokenStorage.storeOAuthToken(OAuthToken.fromJson(config_entry.data["token"]))
-        tempNotificationInfoStorage.storeCredentials(credentials)
-        for persistentId in persistentIds:
-            tempNotificationInfoStorage.storePersistentId(persistentId)
-        
-    if config_entry.version == 2:
-        
-        tempOAuthTokenStorage.storeOAuthToken(OAuthToken.fromJson(config_entry.data["token"]))
-        tempNotificationInfoStorage.storeCredentials(config_entry.data["credentials"])
-        for persistentId in config_entry.data["persistentIds"]:
-            tempNotificationInfoStorage.storePersistentId(persistentId)
-
-    if config_entry.version == 3:
-        tempOAuthTokenStorage.storeOAuthToken(OAuthToken.fromJson(config_entry.options["token"]))
-        tempNotificationInfoStorage.storeCredentials(config_entry.options["credentials"])
-        for persistentId in config_entry.options["persistentIds"]:
-            tempNotificationInfoStorage.storePersistentId(persistentId)
-
-    if config_entry.version < 6:
-        config_entry.version = BlueConConfigFlow.VERSION
-        hass.config_entries.async_update_entry(config_entry, data = {
-                        CONF_CLIENT_ID: "",
-                        CONF_CLIENT_SECRET: ""
-                    }, options = { CONF_LOCK_STATE_RESET: 5 })
-    
-    return True
--- custom_components/bluecon/lock.py
+++ custom_components/bluecon/lock.py
@@ -1,5 +1,5 @@
 import asyncio
-from .const import DEVICE_MANUFACTURER, DOMAIN, CONF_LOCK_STATE_RESET, HASS_BLUECON_VERSION
+from typing import Any, Dict
 from homeassistant.components.lock import LockEntity
 from homeassistant.const import (
     STATE_JAMMED,
@@ -11,27 +11,40 @@ from homeassistant.const import (
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-from bluecon import BlueConAPI
+from .const import DEVICE_MANUFACTURER, DOMAIN, CONF_LOCK_STATE_RESET, HASS_BLUECON_VERSION
+from .fermax_api import FermaxClient
 
 async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry, async_add_entities):
-    bluecon = hass.data[DOMAIN][config.entry_id]
-    lockTimeout = config.options.get(CONF_LOCK_STATE_RESET, 5)
+    client: FermaxClient = hass.data[DOMAIN][config.entry_id]
+    lock_timeout = config.options.get(CONF_LOCK_STATE_RESET, 5)
 
-    pairings = await bluecon.getPairings()
+    pairings = await client.async_get_pairings()
 
     locks = []
 
     for pairing in pairings:
-        deviceInfo = await bluecon.getDeviceInfo(pairing.deviceId)
-        for accessDoorName, accessDoor in pairing.accessDoorMap.items():
+        device_id = pairing["deviceId"]
+        # We can get device info, but pairing has most of it.
+        # Let's try to get more info if needed, but pairing has 'family', 'type', 'subtype' usually?
+        # The script's Pairing class doesn't seem to have family/type.
+        # But get_device_info does.
+        
+        device_info = await client.async_get_device_info(device_id)
+        
+        access_door_map = pairing.get("accessDoorMap", {})
+        
+        for access_door_name, access_door_data in access_door_map.items():
+            if not access_door_data.get("visible", True):
+                continue
+                
             locks.append(
                 BlueConLock(
-                    bluecon,
-                    pairing.deviceId,
-                    accessDoorName,
-                    accessDoor,
-                    deviceInfo,
-                    lockTimeout
+                    client,
+                    device_id,
+                    access_door_name,
+                    access_door_data,
+                    device_info,
+                    lock_timeout
                 )
             )
     
@@ -40,17 +53,20 @@ async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry, async_add_
 class BlueConLock(LockEntity):
     _attr_should_poll = False
 
-    def __init__(self, bluecon: BlueConAPI, deviceId, accessDoorName, accessDoor, deviceInfo, lockTimeout):
-        self.bluecon = bluecon
-        self.lockId = f'{deviceId}_{accessDoorName}'
-        self.deviceId = deviceId
-        self.accessDoorName = accessDoorName
-        self.accessDoor = accessDoor
-        self._attr_unique_id = f'{self.lockId}_door_lock'.lower()
+    def __init__(self, client: FermaxClient, device_id: str, access_door_name: str, access_door_data: Dict[str, Any], device_info: Dict[str, Any], lock_timeout: int):
+        self.client = client
+        self.lock_id = f'{device_id}_{access_door_name}'
+        self.device_id = device_id
+        self.access_door_name = access_door_name
+        self.access_door_data = access_door_data # Contains accessId dict
+        self._attr_unique_id = f'{self.lock_id}_door_lock'.lower()
         self.entity_id = f'{DOMAIN}.{self._attr_unique_id}'.lower()
         self._state = STATE_LOCKED
-        self.__model = f'{deviceInfo.type} {deviceInfo.subType} {deviceInfo.family}',
-        self.__lockTimeout = lockTimeout
+        
+        model = f"{device_info.get('type', '')} {device_info.get('subtype', '')} {device_info.get('family', '')}".strip()
+        self._model = model if model else "Fermax Blue Device"
+        
+        self._lock_timeout = lock_timeout
     
     @property
     def is_locking(self) -> bool:
@@ -72,31 +88,35 @@ class BlueConLock(LockEntity):
         """Return true if lock is locked."""
         return self._state == STATE_LOCKED
 
-    async def async_lock(self) -> None:
+    async def async_lock(self, **kwargs) -> None:
         pass
 
-    async def async_unlock(self) -> None:
+    async def async_unlock(self, **kwargs) -> None:
         """Unlock the device."""
         self._state = STATE_UNLOCKING
-        self.async_schedule_update_ha_state(True)
-        await self.bluecon.openDoor(self.deviceId, self.accessDoor)
+        self.async_write_ha_state()
+        
+        access_id = self.access_door_data["accessId"]
+        await self.client.async_open_door(self.device_id, access_id)
+        
         self._state = STATE_UNLOCKED
-        self.async_schedule_update_ha_state(True)
-        await asyncio.sleep(self.__lockTimeout)
+        self.async_write_ha_state()
+        
+        await asyncio.sleep(self._lock_timeout)
         self._state = STATE_LOCKED
-        self.async_schedule_update_ha_state(True)
+        self.async_write_ha_state()
 
-    async def async_open(self) -> None:
-        pass
+    async def async_open(self, **kwargs) -> None:
+        await self.async_unlock(**kwargs)
     
     @property
     def device_info(self) -> DeviceInfo | None:
         return DeviceInfo(
             identifiers = {
-                (DOMAIN, self.deviceId)
+                (DOMAIN, self.device_id)
             },
-            name = f'{self.__model} {self.deviceId}',
+            name = f'{self._model} {self.device_id}',
             manufacturer = DEVICE_MANUFACTURER,
-            model = self.__model,
+            model = self._model,
             sw_version = HASS_BLUECON_VERSION
         )
